b23b7cd88abfbf62e70a10a38053017e0930e6ad
gfunk77 <faraci77@gmail.com>
2023-11-14 12:27:52 -0500

    add routerLink to template

--- 03-User-Navigation-Between-Views/src/app/header/header.component.ts
+++ 03-User-Navigation-Between-Views/src/app/header/header.component.ts
@@ -3,25 +3,22 @@ import { Category } from '../models/pie';
 import { PieService } from '../services/pie.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { ROUTER_TOKENS } from '../app.routes';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [
-    MatMenuModule,
-    MatButtonModule,
-  ],
+  imports: [MatMenuModule, MatButtonModule, RouterLink],
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  readonly ROUTER_TOKENS = ROUTER_TOKENS;
   readonly Category = Category;
-  constructor(
-    private readonly pieService: PieService
-  ){}
+  constructor(private readonly pieService: PieService) {}
 
-  changeCategory(category: Category){
+  changeCategory(category: Category) {
     this.pieService.setSelectedCategory(category);
   }
-
 }
