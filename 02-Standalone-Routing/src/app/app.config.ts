1dc350e77798e79e0145b6425c4aabe3af8598cc
gfunk77 <faraci77@gmail.com>
2023-11-14 12:02:48 -0500

    bootstrap routing in standalone

--- 02-Standalone-Routing/src/app/app.config.ts
+++ 02-Standalone-Routing/src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimations(),
-  ]
+  providers: [provideAnimations(), provideRouter(ROUTES, withDebugTracing())],
 };
-
-
